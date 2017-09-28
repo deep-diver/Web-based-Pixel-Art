d24efdd07c9b40593775142a876a83d4c56ffe03
chansung park <deep.diver.csp@gmail.com>
2017-09-28 12:10:31 +0900

    make grid.

--- designs.js
+++ designs.js
@@ -3,8 +3,38 @@
 
 // When size is submitted by the user, call makeGrid()
 
+$(function() {
+    $("input[type=submit]").on("click", function(event) {
+        makeGrid();
+        event.preventDefault();
+    });
+});
+
+function clearGrid() {
+    const table = $("#pixel_canvas");
+    table.empty();
+}
+
+function whenMouseOverOnGrid() {
+    $(this).text("1");
+}
+
 function makeGrid() {
+    clearGrid();
 
-// Your code goes here!
+    const width = $("#input_width").val();
+    const height = $("#input_height").val();
+    const table = $("#pixel_canvas");
 
+    for (let row = 0; row < height; row++) {
+        const newTr = $("<tr></tr>");
+
+        for (let col = 0; col < width; col++) {
+            const newCol = $("<th></th>");
+            newCol.on("click", whenMouseOverOnGrid);
+            newTr.append(newCol);
+        }
+
+        table.append(newTr);
+    }
 }
