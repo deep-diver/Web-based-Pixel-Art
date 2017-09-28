25aebde8ea4eb350f3d0e5f63f034f940785dfcd
chansung park <deep.diver.csp@gmail.com>
2017-09-28 16:05:26 +0900

    adding some valuable comments to the source file. removing clearGrid() function since it doesn't do much.

--- designs.js
+++ designs.js
@@ -1,44 +1,59 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
+// when the page is fully loaded and ready
 $(function() {
-    $("input[type=submit]").on("click", function(event) {
-        makeGrid();
-        event.preventDefault();
-    });
+  //when a user hit a button to make a grid
+  $("#sizePicker").submit(function(event) {
+      //this line of code will prevent default browser's behaviour to submit
+      event.preventDefault();
+
+      //make grid
+      makeGrid();
+  });
 });
 
-function clearGrid() {
-    const table = $("#pixel_canvas");
-    table.empty();
-}
-
+/*
+  when one of <th> tags are clicked, whenMouseClickOnGrid() function is called.
+  This function get the color value in Hex from pallette control (#colorPicker),
+  and set the color as the background-color of the <th> tag.
+*/
 function whenMouseClickOnGrid() {
-    const color = $("#colorPicker").val();
-    $(this).css("background-color", color);
+  const color = $("#colorPicker").val();
+  $(this).css("background-color", color);
 }
 
+/*
+  this is where the grid gets drawn.
+  when this function is called, it will append the below structure to the <table> tag.
+
+  <tr>                <- 1st row
+    <th></th>         <- 1st column of the 1st row
+    ..
+  </tr>
+  <tr>                <- 2nd row
+    <th></th>
+    ..
+  </tr>
+  ..
+*/
 function makeGrid() {
-    clearGrid();
+  //before drawing, clear the area for the new grid.
+  const table = $("#pixel_canvas");
+  table.empty();
 
-    const width = $("#input_width").val();
-    const height = $("#input_height").val();
-    const table = $("#pixel_canvas");
+  const width = $("#input_width").val();
+  const height = $("#input_height").val();
 
-    for (let row = 0; row < height; row++) {
-        const newTr = $("<tr></tr>");
+  for (let row = 0; row < height; row++) {
+      const newTr = $("<tr></tr>");
 
-        for (let col = 0; col < width; col++) {
-            const newCol = $("<th></th>");
-            newCol.css("width", "20px");
-            newCol.css("height", "20px");
-            newCol.css("border", "1px solid black");
-            newCol.on("click", whenMouseClickOnGrid);
-            newTr.append(newCol);
-        }
+      for (let col = 0; col < width; col++) {
+          const newCol = $("<th></th>");
+          newCol.css("width", "20px");
+          newCol.css("height", "20px");
+          newCol.css("border", "1px solid black");
+          newCol.on("click", whenMouseClickOnGrid);
+          newTr.append(newCol);
+      }
 
-        table.append(newTr);
-    }
+      table.append(newTr);
+  }
 }
